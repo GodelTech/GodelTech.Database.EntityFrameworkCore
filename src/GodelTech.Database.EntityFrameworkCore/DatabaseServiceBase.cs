d71d07668a01306a7181ef118e722b8b5edb70b0
Vladimir Rodchenko <vladimir@rodchenko.by>
2020-09-13 23:49:56 +0300

    add ApplyDataAsync method

--- src/GodelTech.Database.EntityFrameworkCore/DatabaseServiceBase.cs
+++ src/GodelTech.Database.EntityFrameworkCore/DatabaseServiceBase.cs
@@ -1,7 +1,9 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using GodelTech.Data;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.Extensions.Logging;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -15,6 +17,7 @@ namespace GodelTech.Database.EntityFrameworkCore
     {
         private readonly ILogger _logger;
         private readonly IList<DbContext> _dbContexts;
+        private readonly IDictionary<Type, IDataService> _dataServices;
 
         /// <summary>
         /// Constructor.
@@ -25,6 +28,7 @@ namespace GodelTech.Database.EntityFrameworkCore
         {
             _logger = logger;
             _dbContexts = new List<DbContext>(dbContexts);
+            _dataServices = new Dictionary<Type, IDataService>();
         }
 
         /// <summary>
@@ -49,6 +53,29 @@ namespace GodelTech.Database.EntityFrameworkCore
                 _logger.LogInformation("Delete migrations: {dbContext}", dbContext.GetType().Name);
                 await dbContext.GetService<IMigrator>().MigrateAsync("0");
             }
+        }
+
+        /// <summary>
+        /// Apply data using data services.
+        /// </summary>
+        public async Task ApplyDataAsync()
+        {
+            foreach (var dataService in _dataServices)
+            {
+                await dataService.Value.ApplyDataAsync();
+            }
+        }
+
+        /// <summary>
+        /// Registers data service instance.
+        /// </summary>
+        /// <typeparam name="TEntity">The type of the T entity.</typeparam>
+        /// <typeparam name="TType">The type of the T type.</typeparam>
+        /// <param name="dataService">The data service.</param>
+        protected void RegisterDataService<TEntity, TType>(IDataService dataService)
+            where TEntity : class, IEntity<TType>
+        {
+            _dataServices[typeof(TEntity)] = dataService;
         }
     }
 }
